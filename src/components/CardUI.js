4e1245c11b039b879244841a3192d10b65fafcf4
Kübra Gökduman <67580139+Kubra99@users.noreply.github.com>
2023-07-27 16:08:13 +0300

    cardlar düzenlendi

--- src/components/CardUI.js
+++ src/components/CardUI.js
@@ -1,44 +1,72 @@
 import React from "react";
-import img1 from "../assets/aselsan.png";
-import img2 from "../assets/roketsan.png";
-import img3 from "../assets/tr_egitim.png";
 
 const CardUI = (props) => {
   const style = {
-    marginTop: "25px",
-    border: "1px soft #fefefe",
+    border: "1px solid #F5F5DC",
   };
 
   return (
-    <div className="card">
-      <img
-        src={process.env.PUBLIC_URL + "/aselsan.png"}
-        alt="Aselsan"
-        className="card-img-top"
-      />
-      {/* <img
-        src={process.env.PUBLIC_URL + "/roketsan.png"}
-        alt="Roketsan"
-        className="card-img-top"
-      />
-      <img
-        src={process.env.PUBLIC_URL + "/tr_egitim.png"}
-        alt="Tr Eğitim"
-        className="card-img-top"
-      /> */}
-      <div className="card-body text-dark">
-        <h4 className="card-title">{props.title}</h4>
-        <p className="card-text text-secondary" style={style}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-          necessitatibus nisi amet dolores maiores temporibus voluptatem quaerat
-          adipisci veniam quasi!
-        </p>
-
-        <a href="#" className="btn btn-outline-success">
-          Daha Fazla
-        </a>
+    <div className="mt-8">
+      <div className="grid grid-cols-3 gap-2">
+        <div className="col-span-1"></div>
+        <div className="col-span-1">
+          <div className="card bg-white max-w-md mx-auto rounded-lg overflow-hidden shadow-lg border border-beige">
+            <div className="h-60">
+              <img
+                className="object-cover w-full h-full"
+                src="https://th.bing.com/th/id/OIP.7smhyDV5Trm2SNKpwrX9cgHaBo?w=322&h=77&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+                alt="logo"
+              />
+            </div>
+            <div className="card-body text-dark">
+              <h4 className="card-title text-2xl font-bold">{props.title}</h4>{" "}
+              <p className="card-text text-secondary" style={style}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                necessitatibus nisi amet dolores maiores temporibus voluptatem
+                quaerat adipisci veniam quasi!
+              </p>
+              <div className="flex justify-center">
+                <a
+                  href="#"
+                  className="btn btn-outline-success px-4 py-2 mt-4 inline-block border border-green-500 text-green-500 rounded"
+                >
+                  Daha Fazla
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1">
+          <div className="card bg-white max-w-md mx-auto rounded-lg overflow-hidden shadow-lg border border-beige">
+            <div className="h-60">
+              <img
+                className="object-cover w-full h-full"
+                src="https://th.bing.com/th/id/OIP.7smhyDV5Trm2SNKpwrX9cgHaBo?w=322&h=77&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+                alt="logo"
+              />
+            </div>
+            <div className="card-body text-dark">
+              <h4 className="card-title text-2xl font-bold">{props.title}</h4>{" "}
+              <p className="card-text text-secondary" style={style}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                necessitatibus nisi amet dolores maiores temporibus voluptatem
+                quaerat adipisci veniam quasi!
+              </p>
+              <div className="flex justify-center">
+                <a
+                  href="#"
+                  className="btn btn-outline-success px-4 py-2 mt-4 inline-block border border-green-500 text-green-500 rounded"
+                >
+                  Daha Fazla
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1"></div>
       </div>
     </div>
   );
 };
+
 export default CardUI;
