9479e16cacae48cf900c15e9d4a87bec1680bc29
selayyirtimci <115094667+selayyirtimci@users.noreply.github.com>
2023-08-01 10:18:05 +0300

    klasör adı fix

--- src/screen/Companies/index.js
+++ src/screen/Companies/index.js
@@ -1,1 +1,16 @@
+import React from "react";
+import HeaderMain from  '../../components/HeaderMain'
+import Companies from  '../../components/Companies'
+
+
+
+function index() {
+  return (
+    < div>
+      <HeaderMain/>
+      <Companies/>
+    </div>
+  );
+}
+
 export default index;
