ebce8a4b20fbb5691057d4eef5b6a850f8121c79
Kübra Gökduman <67580139+Kubra99@users.noreply.github.com>
2023-08-01 09:08:33 +0300

    düzenleme

--- src/screen/Companies/index.js
+++ src/screen/Companies/index.js
@@ -1,1 +1,14 @@
+import React from "react";
+import HeaderMain from "../../components/HeaderMain";
+import Companies from "../../components/Companies";
+
+function index() {
+  return (
+    <div>
+      <HeaderMain />
+      <Companies />
+    </div>
+  );
+}
+
 export default index;
