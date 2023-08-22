c9c6ef93b9315720f8de58d7ac6d0e41f60e4f84
yldrm-88 <salihay406@gmail.com>
2023-08-22 22:46:52 +0300

    ilan eşleşme bazı değişiklikler

--- src/screen/Matchup/index.js
+++ src/screen/Matchup/index.js
@@ -82,107 +82,43 @@ function App() {
         </Col>
 
         {/* Middle content column */}
-        <Col xs={7} className="content-column">
-          <div className="grid grid-cols-1 gap-5 p-8"></div>
-          <h1 className="dark:text-white font-bold text-2xl p-2 text-center">
-            SİZİN İÇİN ÖNERİLEN İLANLAR
+        <Col xs={7} className="content-column ">
+          <h1 className="dark:text-white font-bold text-xl p-4 text-center">
+           SİZE UYGUN İLAN EŞLEŞMELERİ
           </h1>
-          <div className="grid grid-cols-1 gap-1 p-8">
-            <div className="bg-white w-full h-45 rounded-lg border-2">
+          <div className="grid grid-cols-1 gap-1 p-8 ">
+            <div className="bg-white w-full h-45 rounded-lg border-2  shadow-lg  hover:shadow-teal-500">
               <div className="bg-white rounded-lg w-full h-45 p-4">
-                <h3 className="text-xl font-semibold text-blue-500">
+                <h2 className="text-xl font-semibold text-blue-500">
                   HAVELSAN
-                </h3>
-                <h4 className="text-l font-semibold text-blue-500">
+                </h2>
+                <h3 className="text-l font-semibold text-blue-500 mt-3">
                   Kıdemli İş Geliştirme Uzmanı
-                </h4>
+                </h3>
               </div>
 
               <div className="bg-white w-full h-45   p-4">
                 <div className="flex mt-2 max-w-[450px]">
-                  <AiOutlineEnvironment className="text-2xl" />
+                  <AiOutlineEnvironment className="text-2xl " />
                   <p className="text-s">Ankara</p>
                 </div>
-                <div className="flex mt-2 max-w-[450px]">
-                  <FaCalendarAlt className="text-2xl" />
+                <div className="flex mt-3 max-w-[450px] ">
+                  <FaCalendarAlt className="text-2xl " />
                   <p className="text-s">01.07.2023-02.09.23</p>
                 </div>
-                <div className="flex mt-2 max-w-[450px]">
+                <div className="flex mt-3 max-w-[450px]">
                   <FaClock className="text-2xl" />
                   <p className="text-s">Tam Zamanlı</p>
                 </div>
-                <div className="flex mt-2 max-w-[450px]">
+                <div className="flex mt-3 max-w-[450px]">
                   <p className="text-s"></p>
                 </div>
               </div>
-
-              <div className="grid grid-cols-2 gap-1 p-4 max-w-[350px] text-center">
-                <div className="bg-light-grey bg-white w-full h-15 rounded-lg-more border-2 center-content">
-                  <div className="flex mt-2 max-w-[750px] text-center">
-                    <FaEye className="text-xl" />
-                    <p className="green text-s">Sonuçları gör</p>
-                  </div>
-                </div>
-
-                <div className="bg-light-grey bg-white w-full h-45 rounded-lg-more border-2 center-content">
-                  <div className="flex mt-2 max-w-[750px] text-center">
-                    <FaClock className="text-xl" />
-                    <p className="text-l">Başvuruldu</p>
-                  </div>
-                </div>
-              </div>
               <div className="bg-white  w-full h-45 border-2">
-                {/*
-                <div className="bg-white rounded-lg w-full h-45 p-4">
-                  <h3 className="text-xl font-semibold text-blue-500">
-                    ASELSAN
-                  </h3>
-                  <h4 className="text-l font-semibold text-blue-500">
-                    Yazılım Geliştirme Uzmanı
-                  </h4>
-                </div>
-              
-                <div className="bg-white w-full h-45 p-4">
-                  <div className="flex mt-2 max-w-[450px]">
-                    <AiOutlineEnvironment className="text-2xl" />
-                    <p className="text-s">Ankara</p>
-                  </div>
-                  <div className="flex mt-2 max-w-[450px]">
-                    <FaCalendarAlt className="text-2xl" />
-                    <p className="text-s">05.08.2023-0.10.23</p>
-                  </div>
-                  <div className="flex mt-2 max-w-[450px]">
-                    <FaClock className="text-2xl" />
-                    <p className="text-s">Yarı Zamanlı</p>
-                  </div>
-                  <div className="flex mt-2 max-w-[450px]">
-                    <p className="text-s"></p>
-                  </div>
-                </div>
-
-                <div className="grid grid-cols-2 gap-1 p-4 max-w-[350px] text-center">
-                  <div className="bg-light-grey bg-white w-full h-15 rounded-lg-more border-2 center-content">
-                    <div className="flex mt-2 max-w-[750px] text-center">
-                      <FaEye className="text-xl" />
-                      <p className="green text-s">Sonuçları gör</p>
-                    </div>
-                  </div>
-
-                  <div className="bg-light-grey bg-white w-full h-45 rounded-lg-more border-2 center-content">
-                    <div className="flex mt-2 max-w-[750px] text-center">
-                      <FaClock className="text-xl" />
-                      <p className="text-l">Başvuruldu</p>
-                    </div>
-                  </div>
-                </div>
-                */}
               </div>
             </div>
           </div>
         </Col>
-        {/* Left empty column */}
-
-        {/* <Col xs={3} /> */}
       </Grid>
       <Footer />
     </div>
